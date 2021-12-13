875191ff9028f5cc8be7878facf9c0e30e60a52a
Dang Hoang Phong <71632629+phongdh228@users.noreply.github.com>
2021-12-13 10:53:40 +0700

    New payment process - test 01

--- Wallet/Wallet/addNewPaymentPage.xaml.cs
+++ Wallet/Wallet/addNewPaymentPage.xaml.cs
@@ -12,6 +12,9 @@ namespace Wallet.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class addNewPaymentPage : ContentPage
     {
+        int money=0;
+        string title = "Chế độ ăn", img = "";
+     
         public addNewPaymentPage()
         {
             InitializeComponent();
@@ -114,7 +117,25 @@ namespace Wallet.Views
 
         private void confirm_Clicked(object sender, EventArgs e)
         {
+            Payment newPayment = new Payment();
+            newPayment.PaymentImg = img;
+            newPayment.PaymentMoney = money.ToString();
+            newPayment.PaymentTitle = title;
+            /*
+            newPayment.PaymentNote = ;
+            newPayment.PaymentTime = ;
+            newPayment.PaymentWallet = ;
+            */
 
+            Database db = new Database();
+            if (db.AddNewPayment(newPayment))
+            {
+                Navigation.PopAsync();
+            }
+            else
+            {
+                DisplayAlert("Thông báo", "Thêm ví thất bại", null, "OK");
+            }
         }
 
         private void tranfer_Clicked(object sender, EventArgs e)
@@ -125,6 +146,102 @@ namespace Wallet.Views
         private void income_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new addNewIncome());
+        }
+
+        private void chedoan_Clicked(object sender, EventArgs e)
+        {
+            title = "Chế độ ăn";
+            img = "sachbotui_expend_01.png";
+        }
+
+        private void hangngay_Clicked(object sender, EventArgs e)
+        {
+            title = "Hàng ngày";
+            img = "sachbotui_expend_03.png";
+        }
+
+        private void giaothong_Clicked(object sender, EventArgs e)
+        {
+            title = "Giao thông";
+            img = "sachbotui_expend_05.png";
+        }
+
+        private void xahoi_Clicked(object sender, EventArgs e)
+        {
+            title = "Xã hội";
+            img = "sachbotui_expend_07.png";
+        }
+
+        private void dancu_Clicked(object sender, EventArgs e)
+        {
+            title = "Dân cư";
+            img = "sachbotui_expend_12.png";
+        }
+
+        private void quatang_Clicked(object sender, EventArgs e)
+        {
+            title = "Quà tặng";
+            img = "sachbotui_expend_13.png";
+        }
+
+        private void giaotiep_Clicked(object sender, EventArgs e)
+        {
+            title = "Giao tiếp";
+            img = "sachbotui_expend_14.png";
+        }
+
+        private void quanao_Clicked(object sender, EventArgs e)
+        {
+            title = "Quần áo";
+            img = "sachbotui_expend_15.png";
+        }
+
+        private void giaitri_Clicked(object sender, EventArgs e)
+        {
+            title = "Giải trí";
+            img = "sachbotui_expend_20.png";
+        }
+
+        private void sacdep_Clicked(object sender, EventArgs e)
+        {
+            title = "Sắc đẹp";
+            img = "sachbotui_expend_21.png";
+        }
+
+        private void ykhoa_Clicked(object sender, EventArgs e)
+        {
+            title = "Y khoa";
+            img = "sachbotui_expend_22.png";
+        }
+
+        private void thue_Clicked(object sender, EventArgs e)
+        {
+            title = "Thuế";
+            img = "sachbotui_expend_23.png";
+        }
+
+        private void giaoduc_Clicked(object sender, EventArgs e)
+        {
+            title = "Giáo dục";
+            img = "sachbotui_expend2_03.png";
+        }
+
+        private void treem_Clicked(object sender, EventArgs e)
+        {
+            title = "Trẻ em";
+            img = "caidat_thanhvien_14.png";
+        }
+
+        private void thunuoi_Clicked(object sender, EventArgs e)
+        {
+            title = "Thú nuôi";
+            img = "sachbotui_expend2_07.png";
+        }
+
+        private void dulich_Clicked(object sender, EventArgs e)
+        {
+            title = "Du lịch";
+            img = "sachbotui_expend2_09.png";
         }
     }
 }
