56b4743d42cafee6fab6581e6a1ccef9542c8b02
Anthony Hai <namsaoquan@gmail.com>
2021-12-30 11:12:04 +0700

    Update colorful icon in tabbed page

--- Wallet/Wallet.Android/MainActivity.cs
+++ Wallet/Wallet.Android/MainActivity.cs
@@ -10,9 +10,13 @@ using Wallet.Droid;
 using Android.Content;
 using Android.Graphics.Drawables;
 using Android.Text;
+using Xamarin.Forms.Platform.Android.AppCompat;
+using Android.Views;
+using Google.Android.Material.BottomNavigation;
 
 [assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
 [assembly: ExportRenderer(typeof(Editor), typeof(EditorRendererAndroid), new[] { typeof(VisualMarker.DefaultVisual) })]
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(MyTabbedRenderer))]
 namespace Wallet.Droid
 {
 
@@ -76,6 +80,35 @@ namespace Wallet.Droid
             {
                 Control.Background = null;
                 Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+
+    public class MyTabbedRenderer : TabbedPageRenderer
+    {
+        public MyTabbedRenderer(Context context) : base(context)
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<TabbedPage> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null && e.NewElement != null)
+            {
+                for (int i = 0; i <= this.ViewGroup.ChildCount - 1; i++)
+                {
+                    var childView = this.ViewGroup.GetChildAt(i);
+                    if (childView is ViewGroup viewGroup)
+                    {
+                        for (int j = 0; j <= viewGroup.ChildCount - 1; j++)
+                        {
+                            var childRelativeLayoutView = viewGroup.GetChildAt(j);
+                            if (childRelativeLayoutView is BottomNavigationView)
+                            {
+                                ((BottomNavigationView)childRelativeLayoutView).ItemIconTintList = null;
+                            }
+                        }
+                    }
+                }
             }
         }
     }
